cec9a45866ed1a27713f044fb43dec70f472f7a6
proteriax <8125011+proteriax@users.noreply.github.com>
2020-07-26 21:40:00 -0400

    #4

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const emptyFileName = "\0rollup_plugin_ignore_empty_module_placeholder"
 function ignore(list, options = {}) {
   return {
     resolveId(importee) {
-      return list.includes(importee) ? emptyFileName : null
+      return list.includes(importee) ||
+        (options.commonjsBugFix &&
+          list.includes(importee.replace(/^\0/, "").replace(/\?commonjs-proxy$/, "")))
+        ? emptyFileName
+        : null
     },
     load(id) {
-      return (options.commonjsBugFix ? id.includes(emptyFileName) : id === emptyFileName)
-        ? emptyFile
-        : null
+      return id === emptyFileName ? emptyFile : null
     },
   }
 }
