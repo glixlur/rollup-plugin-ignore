5e5418f457cdd1173dc460076d0ad5745eebca93
proteriax <8125011+proteriax@users.noreply.github.com>
2020-07-29 23:03:29 -0400

    #4

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const emptyFileName = "\0rollup_plugin_ignore_empty_module_placeholder"
 function ignore(list, options = {}) {
   return {
     resolveId(importee) {
-      return list.includes(importee) ||
+      return importee === emptyFileName ||
+        list.includes(importee) ||
         (options.commonjsBugFix &&
           list.includes(importee.replace(/^\0/, "").replace(/\?commonjs-proxy$/, "")))
         ? emptyFileName
